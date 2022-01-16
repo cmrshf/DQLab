3ca824ecf4a4dcc27404a1b89848b67f230d47f9
Chika Marsha F <cmrshf@gmail.com>
2022-01-17 01:56:17 +0700

    Create Data Preparation in Data Science using R.R
    create

---
+++ Learn/R/Fundamental/Data Preparation in Data Science using R/Data Preparation in Data Science using R.R
@@ -0,0 +1,31 @@
+# NA DAN DEFAULT TYPE
+# Ketik nilai NA
+NA
+# Tampilkan type dari NA dengan function typeof
+typeof(NA)
+
+
+
+# MENGGUNAKAN FUNCTION IS.NA
+# Buat variable x yang diisi dengan nilai NA
+x <- NA
+# Pengecekan variable x dengan nilai NA menggunakan operator ==
+x == NA
+# Pengecekan variable x dengan nilai NA menggunakan function is.na
+is.na(x)
+
+
+
+# VARIASI NA DAN IS.NA
+typeof(NA_integer_)
+typeof(NA_real_)
+typeof(NA_complex_)
+typeof(NA_character_)
+is.na(NA_integer_)
+is.na(NA_real_)
+is.na(NA_complex_)
+is.na(NA_character_)
+
+
+
+# COERCION PADA VECTOR YANG MENGANDUNG NA
