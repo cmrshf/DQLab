fb5533fb27a572c8157f04baf66e5bad92c4f1b3
Chika Marsha F <cmrshf@gmail.com>
2022-01-21 15:26:26 +0700

    Update Data Preparation in Data Science using R.R
    update

--- Learn/R/Fundamental/Data Preparation in Data Science using R/Data Preparation in Data Science using R.R
+++ Learn/R/Fundamental/Data Preparation in Data Science using R/Data Preparation in Data Science using R.R
@@ -29,3 +29,64 @@ is.na(NA_character_)
 
 
 # COERCION PADA VECTOR YANG MENGANDUNG NA
+# Membuat vector bernama isi.vector dengan isi bilangan, dimana salah satunya memiliki missing value
+isi.vector <- c(1, 2, 3, NA, 3, 1)
+# Mengecek keseluruhan tipe data dengan perulangan lapply dan typeof
+lapply(isi.vector, typeof)
+# Menggunakan is.na untuk mengecek keberadaan missing value dari tiap elemen pada vector
+is.na(isi.vector)
+
+
+
+# NULL DAN VECTOR
+# Membuat vector dengan 7 elemen termasuk NA dan NULL
+isi.vector <- c(1, 2, 3, NA, 5, NULL, 7)
+# Menghitung jumlah elemen dari isi.vector
+length(isi.vector)
+
+
+
+# NULL DAN LIST
+# Membuat list dengan 3 elemen termasuk NA dan NULL
+isi.list <- list(1, NULL, 3, NA, 5)
+# Menghitung jumlah elemen dari isi.list
+length(isi.list)
+
+
+
+# INF UNTUK MEWAKILI INFINITE NUMBER
+# Hitung kalkulasi 5 dibagi dengan 0
+5/0
+# Hitung kalkulasi -120 dibagi dengan 0
+-120/0
+
+
+
+# NaN (Not a Number)
+# Hitung kalkulasi 0 dibagi dengan 0
+0/0
+
+
+
+# NaN DARI HASIL FUNCTION LOG()
+# Hitung logaritma dari angka -1000
+log(-1000)
+
+
+
+# FUNGSI IS.NAN
+# Buat variable contoh.nan
+contoh.nan <- 0/0
+# Periksa dengan function is.nan
+is.nan(contoh.nan)
+
+
+
+# NaN dan is.na VERSUS NA dan is.nan
+# Masukkan code di bawah ini sesuai permintaan soal
+is.na(NaN)
+is.nan(NA)
+
+
+
+# MENGHITUNG JUMLAH MISSING VALUES DARI SATU VECTOR
